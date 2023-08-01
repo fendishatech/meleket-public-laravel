352d56b81d6de80002d6b6acc5390335cb21456f
fendishatech <liyukibir@gmail.com>
2023-08-01 07:15:45 -0700

    header, footer

--- resources/views/master/partials/footer.blade.php
+++ resources/views/master/partials/footer.blade.php
@@ -1,3 +1,12 @@
-<footer class="bg-yellow-300 h-[300px]">
-    <h1>Footer section</h1>
+<footer class="px-6 py-8 bg-gray-700 h-[300px]">
+    {{-- Logo --}}
+    <div class="flex flex-col">
+        <img src="/images/logo/basic-logo.png" class="w-[144px] h-[144px]" alt="Logo">
+        <p>
+            We also believe that it is not enough to be just intelligent in many areas but that children should grow up
+            with good and strong values.
+        </p>
+    </div>
+    {{-- Quick Links --}}
+
 </footer>
--- resources/views/master/partials/header.blade.php
+++ resources/views/master/partials/header.blade.php
@@ -2,7 +2,7 @@
     {{-- Banner --}}
     <div class="p-4 w-full flex justify-between items-center">
         {{-- Addrss --}}
-        <div class="flex gap-4">
+        <div class="flex gap-4 text-blue-900">
             <div class="flex gap-2 items-center">
                 <i class="fa-solid fa-location-dot"></i>
                 <p>Addis Ababa Ethiopia</p>
@@ -18,18 +18,20 @@
         </div>
         {{-- Social --}}
         <div>
-            <i class="fa-brands fa-facebook"></i>
-            <i class="fa-brands fa-twitter"></i>
+            <i
+                class="fa-brands fa-facebook text-2xl p-1 rounded-full text-blue-900 hover:text-white hover:bg-blue-900"></i>
+            <i
+                class="fa-brands fa-twitter text-2xl p-1 rounded-full text-blue-900 hover:text-white hover:bg-blue-900"></i>
         </div>
     </div>
     {{-- Navbar --}}
     <nav class="px-24 py-6 bg-blue-900 flex justify-between">
         {{-- Logo --}}
-        <div class="flex items-center ">
-            <img src="" alt="Logo">
+        <div class="flex items-center">
+            <img src="/images/logo/basic-logo.png" class="w-[81px] h-[81px]" alt="Logo">
             <div class="flex flex-col">
-                <p class="text-white">Meleket school</p>
-                <p class="text-white">መለከት ትምህርት ቤት</p>
+                <p class="text-white text-xl">Meleket school</p>
+                <p class="text-white text-xl">መለከት ትምህርት ቤት</p>
             </div>
         </div>
         {{-- Links --}}
