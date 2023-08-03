1b3541476b76018e247b765b8feede607d3daacd
fendishatech <liyukibir@gmail.com>
2023-08-03 07:38:46 -0700

    first deploy

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -2,5 +2,58 @@
 
 
 @section('content')
-    <h1>Contact us Page</h1>
+    <h1 class="py-6 my-6 text-center text-4xl text-black font-bold">Contact Us</h1>
+    <section class="text-gray-600 body-font relative">
+        <div class="container px-5 py-24 mx-auto flex sm:flex-nowrap flex-wrap">
+            <div
+                class="lg:w-2/3 md:w-1/2 bg-gray-300 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
+
+
+                <iframe class="absolute inset-0"
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15760.639075181412!2d38.709394283576636!3d9.04918720433377!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x164b89cc530afe19%3A0xcd8ae469755fc35!2sMeleket%20School!5e0!3m2!1sen!2set!4v1691073089016!5m2!1sen!2set"
+                    width="100%" height="100%" title="map" marginheight="0" marginwidth="0" style="border:0;"
+                    frameborder="0" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                <div class="bg-white relative flex flex-wrap py-6 rounded shadow-md">
+                    <div class="lg:w-1/2 px-6">
+                        <h2 class="title-font font-semibold text-gray-900 tracking-widest text-xs">ADDRESS</h2>
+                        <p class="mt-1">Nearby Wereda 8 police station, Gulele subcity, woreda 9, Addis Ababa, Ethiopia
+                        </p>
+                    </div>
+                    <div class="lg:w-1/2 px-6 mt-4 lg:mt-0">
+                        <h2 class="title-font font-semibold text-gray-900 tracking-widest text-xs">EMAIL</h2>
+                        <a class="text-indigo-500 leading-relaxed">meleketschool@gmail.com</a>
+                        <h2 class="title-font font-semibold text-gray-900 tracking-widest text-xs mt-4">PHONE</h2>
+
+                        <p class="leading-relaxed">+251 118 677787</p>
+                        <p class="leading-relaxed">+251 118 238444</p>
+                        <p class="leading-relaxed">+251 911 651652</p>
+                        <p class="leading-relaxed">+251 947 405757</p>
+                    </div>
+                </div>
+            </div>
+            <div class="lg:w-1/3 md:w-1/2 bg-white flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+                <h2 class="text-gray-900 text-lg mb-1 font-medium title-font">Feedback</h2>
+                <p class="leading-relaxed mb-5 text-gray-600">Tell Us What you
+                </p>
+                <div class="relative mb-4">
+                    <label for="name" class="leading-7 text-sm text-gray-600">Name</label>
+                    <input type="text" id="name" name="name"
+                        class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                </div>
+                <div class="relative mb-4">
+                    <label for="email" class="leading-7 text-sm text-gray-600">Email</label>
+                    <input type="email" id="email" name="email"
+                        class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                </div>
+                <div class="relative mb-4">
+                    <label for="message" class="leading-7 text-sm text-gray-600">Message</label>
+                    <textarea id="message" name="message"
+                        class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+                </div>
+                <button
+                    class="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Send</button>
+
+            </div>
+        </div>
+    </section>
 @endsection
